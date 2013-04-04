ae4298ca0d86d54b88bd3cae321849dee2903864
Lars Haugan <lars@larshaugan.net>
2013-04-04 14:13:56 +0200

    Added comments to player class

--- class_player.h
+++ class_player.h
@@ -7,15 +7,15 @@
 namespace player{
     class player{
         private:
-            int id;
-            char sign;
-            std::string name;
+            int id;                             // The users ID
+            char sign;                          // The sign of the user (eg. 'X' or '0')
+            std::string name;                   // The username
         public:
-            player(int, char);
-            player(int, char, std::string);
-            int getId();
-            char getSign();
-            std::string getName();
+            player(int, char);                  // Constructor. Sets userID and sign
+            player(int, char, std::string);     // Constructor. Sets userID, sign and the username
+            int getId();                        // Returns the users ID
+            char getSign();                     // Returns the sign of the user
+            std::string getName();              // Returns the name of the user
     };
 }
 
