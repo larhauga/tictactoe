4cac46036c4f5a7566c5cd536fc7cb2ce607189f
Lars Haugan <lars@larshaugan.net>
2013-04-04 14:14:11 +0200

    Added comments to space class

--- class_space.h
+++ class_space.h
@@ -13,19 +13,19 @@ namespace space{
     //The space is where you can place a chip
     class space{
         private:
-            t_coord xCoord;
-            t_coord yCoord;
-            t_chip userChip;
-            int userId;
+            t_coord xCoord;             // The x coordinate
+            t_coord yCoord;             // The y coordinate
+            t_chip userChip;            // Users chip (eg. 'X' or '0')
+            int userId;                 // The users ID
         public:
-            space(t_coord, t_coord);
-            t_coord getxCoord();
-            t_coord getyCoord();
-            bool setChip(t_chip);
-            bool setChip(t_chip, int);
-            t_chip getChip();
-            bool isUsed();
-            int getUserId();
+            space(t_coord, t_coord);    // Construcotr. Called when initializing the board
+            t_coord getxCoord();        // Getting the x coordinate from space.
+            t_coord getyCoord();        // Getting the y coordinate from space.
+            bool setChip(t_chip);       // Sets the chip to the board.
+            bool setChip(t_chip, int);  // Sets the chip AND the userID
+            t_chip getChip();           // Returns the chip of the board. Std. is '#'
+            bool isUsed();              // Returns true if occupied. False if not
+            int getUserId();            // Returns the userID of the user whom this space belongs to
     };
 }
 
